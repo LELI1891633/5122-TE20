3a27d3cb3ff3c8f4663175770fde86f07e72f54f
LELI <llii0202@student.monash.edu>
2025-08-11 20:47:16 +1000

    chore: stop tracking node_modules/dist and env

--- node_modules/server/dist/middlewares/error-handler.js
+++
@@ -1,9 +0,0 @@
-export function errorHandler(err, _req, res, _next) {
-    const status = err.status ?? 500;
-    const code = err.code ?? 'InternalError';
-    const message = err.message ?? 'Unexpected error';
-    if (process.env.NODE_ENV !== 'test')
-        console.error(err);
-    res.status(status).json({ error: { code, message } });
-}
-//# sourceMappingURL=error-handler.js.map
